35a5a347a0897101d32f77a143ac7fb399e49960
vinskn <chuncumaru1@gmail.com>
2024-04-22 21:30:18 +0700

    Landing tampilan utama

--- main.cpp
+++ main.cpp
@@ -1,1 +1,64 @@
 // Menu Utama
+
+
+// Menu Utama
+
+#include <iostream>
+
+using namespace std;
+
+void daftar_pilihan () 
+{
+    cout<<endl
+        << "Pilihan Menu" << endl
+        << "1. Daftar Reservasi" << endl
+        << "2. Tambah Reservasi" << endl
+        << "3. Hapus Reservasi"<< endl
+        << "4. Keluar"
+        << endl;
+}
+
+
+int main () {
+    cout << endl;
+    cout << "==========================" << endl;
+    cout << "     Selamat Datang" << endl;
+    cout << "Sistem Reservasi Restoran" << endl;
+    cout << "==========================" << endl;
+    cout << endl;
+
+    daftar_pilihan();
+    int pilih;
+
+    cout << "Pilihan Anda (Pilih Nomor) : "; cin >> pilih;
+    
+    while (pilih != 4){
+        if (pilih == 1)
+        {
+            // Modul daftar reservasi
+            cout << "1 pass" << endl;
+            daftar_pilihan();
+            cout << "Silahkan pilih kembali nomor Anda : "; cin >> pilih;
+        }
+
+        else if (pilih == 2)
+        {
+            // modul tambah reservasi
+            cout << "2 pass" << endl;
+            daftar_pilihan();
+            cout << "Silahkan pilih kembali nomor Anda : "; cin >> pilih;
+        }
+
+        else if (pilih == 3)
+        {
+            // modul hapus reservasi
+            cout << "3 pass" << endl;
+            daftar_pilihan();
+            cout << "Silahkan pilih kembali nomor Anda : "; cin >> pilih;
+        }
+        
+    }
+   
+   cout << "Terima Kasih telah menggunakan sistem ini";
+    
+}
