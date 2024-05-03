a993c3aec5c48c573bb008bab2cce6d7e4970b9a
vinskn <chuncumaru1@gmail.com>
2024-05-03 22:27:01 +0700

    none

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,16 +1,60 @@
 #include <iostream>
-#include "log.h"
-#include "tambah.h"
+#include <string>
+#include <fstream>
 
 using namespace std;
 
-void daftar_pilihan () 
+struct show_entry
 {
-    cout<<endl
-        << "Pilihan Menu" << endl
-        << "1. Daftar Reservasi" << endl
-        << "2. Tambah Reservasi" << endl
-        << "3. Hapus Reservasi"<< endl
-        << "4. Keluar"
-        << endl;
+    string nme;
+    int jml;
+    string tgl;
+    string wkt;
+};
+
+show_entry *first = nullptr;
+
+bool check()
+{
+    if (first == nullptr)
+    {
+        return true;
+    }
+    return false;
+}
+
+void LL_input ()
+{
+    ifstream file ("reservasi.csv");
+    if (file.fail())
+    {
+        cout << "Gagal membuka file";
+        return;
+    }
+
+    while (!file.eof())
+    {
+        show_entry *node;
+        node = new show_entry;
+
+        string d1;
+        int d2;
+        string d3;
+        string d4;
+        string tmp;
+        getline (file, d1, ',');
+        getline (file, tmp, ',');
+        d2 = atoi(tmp.c_str());
+        getline (file, d3, ',');
+        getline (file, d4, '\n');
+
+        cout << d1 << endl;
+
+    }
+}
+
+
+int main ()
+{
+    show_entry();
 }
